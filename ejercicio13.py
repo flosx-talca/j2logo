a6c8a2796b298dc525c30515cf8befecfe5d14bc
flosx-talca <orlandorozasi@gmail.com>
2020-08-11 13:20:24 -0400

    Ejercicio 13 con comentario

--- ejercicio13.py
+++ ejercicio13.py
@@ -4,7 +4,6 @@ def acumulado(lista):
         lista[i]=lista[i]+lista[i-1]
     return lista
 
-
-lista = [random.randint(1, 5)for i in range(5)]
+lista = [random.randint(1, 5)for i in range(5)] #genera lista random
 print(f"Lista inicial: {lista}")
 print (f"Lista final: {acumulado(lista)}")
